62753b871b898a2dd44fdb78d6b89302676fb7e7
zenos <ericcampbell294@gmail.com>
2022-09-18 22:27:58 +0000

    use env vars

--- src/database/pg.ts
+++ src/database/pg.ts
@@ -1,9 +1,11 @@
 import { Pool } from 'pg';
+import dotenv from 'dotenv';
+dotenv.config();
 
 export const pool = new Pool({
-  user: 'chimera',
+  user: `${process.env.DB_USERNAME}`,
   host: 'localhost',
   database: 'exercise_tracker',
-  password: '',
+  password: `${process.env.DB_PASSWORD}`,
   port: 5432
 })
